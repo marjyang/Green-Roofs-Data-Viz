fba6e911e52347bfdf5af3edfa0fdc04b4e607fa
qiqizhouu02 <yz6897@nyu.edu>
2023-12-10 14:08:22 -0500

    css-changes

--- scripts/leaflet-integration.js
+++ scripts/leaflet-integration.js
@@ -1,6 +1,68 @@
 var map = L.map('map').setView([40.785091, -73.968285], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
+    maxZoom: 80,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Function to add markers to the map
+function addMarkers(data) {
+    data.forEach(point => {
+        var marker = L.marker([parseFloat(point.lat), parseFloat(point.lng)]);
+        // Configure and add marker to the map
+        // Create popup content string
+        var popupContent = `
+              <div>
+                  <h4>ZIP Code: ${point.zip}</h4>
+                  <p>Veg Coverage: ${point.vegcoverage}%</p>
+                  <p>Summer Temperature: ${point.summertemp}°F</p>
+                  <p>Walkability: ${point.walkability}/100</p>
+              </div>
+          `;
+
+        // Bind popup to marker
+        marker.bindPopup(popupContent);
+
+        marker.addTo(map);
+    });
+}
+
+// Global variable to store JSON data
+var jsonData = [];
+
+// Load JSON data
+fetch('final-merged.json')
+    .then(response => response.json())
+    .then(data => {
+        jsonData = data; // Store data globally
+    })
+    .catch(error => console.error('Error loading JSON data:', error));
+
+// Event listener for the search button
+document.getElementById('search-button').addEventListener('click', function() {
+    var inputZip = document.getElementById('zip-code-input').value;
+    searchByZipCode(inputZip);
+});
+
+// Function to search and display markers by zip code
+function searchByZipCode(zipCode) {
+    // Filter data for the entered zip code
+    var filteredData = jsonData.filter(point => point.zip === zipCode);
+
+    if (filteredData.length > 0) {
+        // Clear existing markers
+        map.eachLayer(function(layer) {
+            if (!!layer.toGeoJSON) {
+                map.removeLayer(layer);
+            }
+        });
+
+        // Add new markers
+        addMarkers(filteredData);
+
+        // Optionally, set the map view to the first matched location
+        map.setView([parseFloat(filteredData[0].lat), parseFloat(filteredData[0].lng)], 13);
+    } else {
+        console.log('No data found for the entered zip code');
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -19,15 +19,15 @@ function loadData() {
 
 function displayResults(data) {
     var resultContainer = document.getElementById('result-container');
-    
+
     if (data.length > 0) {
         let htmlContent = '<ul>';
         data.forEach(row => {
-            htmlContent += `<li>you live in neighborhood: ${row.geography}</li>`; // Replace 'geogrpahy' with the actual property name
-            htmlContent += `<li>the temperuature you experience in the summers are: ${row.summertemp} F</li>`
-            htmlContent += `<li>this must be because the vegetation cover in your neighborhood is only ${row.vegcoverage}%!</li>`
-            htmlContent += `<li>according to an NYT article, with more vegetation, and less concrete, your temperature could be ${row.summertemp-9.5} F</li>`
-            htmlContent += `<li>your walkability to a park is approximately: ${row.walkability} %.</li>` 
+            htmlContent += `<li>You live in neighborhood: ${row.geography}</li>`; // Replace 'geogrpahy' with the actual property name
+            htmlContent += `<li>The temperuature you experience in the summers are: ${row.summertemp} F</li>`
+            htmlContent += `<li>This must be because the vegetation cover in your neighborhood is only ${row.vegcoverage}%!</li>`
+            htmlContent += `<li>According to an NYT article, with more vegetation, and less concrete, your temperature could be ${row.summertemp-9.5} F</li>`
+            htmlContent += `<li>your walkability to a park is approximately: ${row.walkability} %.</li>`
             htmlContent += `<li>wouldn't it be amazing if you also had a green space right above you?</li>`
         })
         htmlContent += '</ul>';
